a98354585a7bd2d8a2b24f7947047a068d7409d2
Archont1 <sevcovsvatoslav588@gmail.com>
2026-07-17 16:36:47 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,22 @@
-hi ?
+android {
+    // ... остальные настройки
+
+    buildFeatures {
+        // Включает детерминированную сборку (AGP 7.0+)
+        // Тогда порядок файлов и метаданные будут фиксированными
+    }
+
+    // Чтобы сборка была предсказуемой
+    android.buildFeatures.deterministicBuild = true
+
+    
+    signingConfigs {
+        create("release") {
+            
+        }
+    }
+
+    // Отключаем сжатие ресурсов
+    isMinifyEnabled = false
+    isShrinkResources = false
+}
