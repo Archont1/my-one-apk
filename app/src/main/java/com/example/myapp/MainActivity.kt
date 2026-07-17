446632b3df40a59a7619a19aeafde4a223ab15cf
Archont1 <sevcovsvatoslav588@gmail.com>
2026-07-17 15:49:31 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -6,10 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val textView = findViewById<TextView>(R.id.textView)
-        textView.text = "A"
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        val textView = findViewById<TextView>(R.id.textView);
+        textView.text = "A";
     }
 }
