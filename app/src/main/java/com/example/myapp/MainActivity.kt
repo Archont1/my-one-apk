8404fa17d21edc52b213c3aa9304f0f95f0b7fa9
Archont1 <sevcovsvatoslav588@gmail.com>
2026-07-17 16:19:01 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         val textView = findViewById<TextView>(R.id.textView);
-        textView.text = "A";
+        textView.text = "Ach";
     }
 }
